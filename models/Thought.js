06a8c2e6b5f81b9d5d1fdc908e8977d3ce82b7ad
Kona <landon.deroin@gmail.com>
2023-08-24 22:13:38 -0400

    thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,66 @@
 const {Schema, model, Type } = require('mongoose');
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAt => dateFormat(createdAt)
+        }
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAt => dateFormat(createdAt)
+        },
+        username: {
+            type: String,
+            required: true,
+            ref: 'User'
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+ThoughtSchema.virtuals('reactionCount').get(function(){
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
