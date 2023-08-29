e12e0bc41aeae3812e9df7a32513dfa08623bac9
Kona <landon.deroin@gmail.com>
2023-08-29 18:53:04 -0400

    added user routes fixed thought routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -8,4 +8,4 @@ router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought
 
 router.route('/:thoughtId').post(addReaction).delete(deleteReaction);
 
-module.exports = router:
+module.exports = router;
