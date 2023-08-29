f522ad0c7d18e6765f5146758b16c9e98c5de599
Kona <landon.deroin@gmail.com>
2023-08-29 18:48:22 -0400

    added thought routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const {
+    getAllThought, getThoughtById, createThought, updateThought, deleteThought, addReaction, deleteReaction} = require("../../controllers/thoughtController")
+
+router.route('/').get(getAllThought).post(createThought);
+
+router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId').post(addReaction).delete(deleteReaction);
+
+module.exports = router:
